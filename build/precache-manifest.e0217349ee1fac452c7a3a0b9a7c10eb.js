dc8d381c3d2e8ed779f39dfaaacbe3b0358369f9
defigods <defigodss@gmail.com>
2020-03-18 21:37:42 -0400

    Fix reload and pagination

---
+++ build/precache-manifest.e0217349ee1fac452c7a3a0b9a7c10eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea81ad03adfef297365474bd1792dd7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "93c2c1b3cd16ae682467",
+    "url": "./static/css/2.913bfbdb.chunk.css"
+  },
+  {
+    "revision": "9053ccee8d2215572b9b",
+    "url": "./static/css/main.6145911d.chunk.css"
+  },
+  {
+    "revision": "93c2c1b3cd16ae682467",
+    "url": "./static/js/2.c35bd898.chunk.js"
+  },
+  {
+    "revision": "50eef3986410e41c0c5c594aa5805fa3",
+    "url": "./static/js/2.c35bd898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9053ccee8d2215572b9b",
+    "url": "./static/js/main.e1640522.chunk.js"
+  },
+  {
+    "revision": "217b2ca1b46df3832976",
+    "url": "./static/js/runtime-main.8388affd.js"
+  },
+  {
+    "revision": "d82e0b14dc7a3037881eb7f0ffd3bcfe",
+    "url": "./static/media/icon_notification.d82e0b14.svg"
+  },
+  {
+    "revision": "eb26a6e4f3ccea9df92613d91674b29c",
+    "url": "./static/media/logo_ovr_white.eb26a6e4.svg"
+  }
+]);
