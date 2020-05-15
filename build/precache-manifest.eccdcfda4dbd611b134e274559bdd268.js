3c7e56a566a1b7b45d10a0214d3425d660c8f78c
defigods <defigodss@gmail.com>
2020-05-15 10:58:15 -0400

    build

---
+++ build/precache-manifest.eccdcfda4dbd611b134e274559bdd268.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78afd84b488bfeaaa956e70c3fa3e157",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2958e0737dbee73fdafa",
+    "url": "./static/css/2.c6629948.chunk.css"
+  },
+  {
+    "revision": "e68221f27d2ef6cb6a81",
+    "url": "./static/css/main.9c4a6112.chunk.css"
+  },
+  {
+    "revision": "2958e0737dbee73fdafa",
+    "url": "./static/js/2.672db935.chunk.js"
+  },
+  {
+    "revision": "d1a1d77e63cd68895bdbf5ecb1b37495",
+    "url": "./static/js/2.672db935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e68221f27d2ef6cb6a81",
+    "url": "./static/js/main.6790cc46.chunk.js"
+  },
+  {
+    "revision": "5e1fcaa02bbc16d6a1da",
+    "url": "./static/js/runtime-main.c6da2750.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.871c6847c6c7d878587967fa6d09490e.js"
+  "./precache-manifest.eccdcfda4dbd611b134e274559bdd268.js"
 );
 
 self.addEventListener('message', (event) => {
