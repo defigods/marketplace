a904f189e07f53e32fcf2017d72d4fc230c94589
defigods <defigodss@gmail.com>
2020-07-04 17:02:45 -0400

    build

---
+++ build/precache-manifest.ec867327901027475e52dfaa773c5d04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9c9e7218e438e1005b0b02307131f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "835318ef73992f98dd38",
+    "url": "./static/css/2.7891b4e2.chunk.css"
+  },
+  {
+    "revision": "bbabb90ffca52be20e93",
+    "url": "./static/css/main.78ea713f.chunk.css"
+  },
+  {
+    "revision": "835318ef73992f98dd38",
+    "url": "./static/js/2.d9d03b01.chunk.js"
+  },
+  {
+    "revision": "dbd865164fd1c7dd90df646d00368d91",
+    "url": "./static/js/2.d9d03b01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbabb90ffca52be20e93",
+    "url": "./static/js/main.9c226d3d.chunk.js"
+  },
+  {
+    "revision": "3195cdf6ee62f63506df",
+    "url": "./static/js/runtime-main.a034842e.js"
+  }
+]);
