07b5408277f0a2753e1a8da667b633d0fe7e8521
defigods <defigodss@gmail.com>
2020-07-18 17:06:30 +0200

    Updated contracts with fixed for ETH participation

--- src/lib/contracts.js
+++ src/lib/contracts.js
@@ -1,9 +1,9 @@
 // TODO change this for the final addresses these are the deployed ropsten ones
-export const daiAddress = '0x8c0454FC85EA81077cd548b65F709a9a0Bb1DA6b';
-export const usdcAddress = '0x0b2280451982847cE642344841A5F66d32eF0e0C';
-export const tetherAddress = '0xf7de5d464656CB33bC8c31Ef67B1276e806c5472';
-export const ovr721Address = '0x289a769beDcbB97c5B0F0079Ae9BB24084CAA31D';
-export const ovrAddress = '0x5BbF4117a72C1e8008dEdce3568725EF09D932b3';
-export const tokenBuyAddress = '0x7B231436D1C937313757840027C8BB23D8dC7b79';
-export const icoAddress = '0x65a006BfD68E4eeBc0dd3a635fD51be98922E890';
-export const icoParticipateAddress = '0xB52E3f62d71eE7d4Cc15a0B9a3a7A58edaAFC895';
+export const daiAddress = '0x1a01Ba6FD4076146FFCbfA1CA92eE01d320eD6FB';
+export const usdcAddress = '0xE0c3e745343883A2884F28747175c4D1e7F47064';
+export const tetherAddress = '0x4EBCAffbE30d00f657a197dF79B44349BCEB4960';
+export const ovr721Address = '0xF591c7CA8d7dE04D5B994fa31Dc626AD9831D4A0';
+export const ovrAddress = '0xfF7D6Ad416877e7d30c5B6C5E45b493e4Bb731D8';
+export const tokenBuyAddress = '0x3cC70d8A647bca74269E3665B8324da40a8b76d0';
+export const icoAddress = '0x318d6A93E5AA7E58A243970AC452708C4401d98A';
+export const icoParticipateAddress = '0x0064F1a86A6c916C9E8703f430b52A3714d806C2';
