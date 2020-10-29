194995a231c6dcc47ff7bea9b2eb92330b25b533
defigods <defigodss@gmail.com>
2020-10-29 10:55:55 +0100

    WIP translation functioning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.scss';
+import './i18n';
 import 'react-notifications-component/dist/theme.css';
 
 // import Home from './views/Home/Home';
@@ -27,62 +28,64 @@ import { Web3Provider, Web3Context } from './context/Web3Context';
 
 function App() {
 	return (
-		<UserProvider>
-			<Web3Provider>
-				<MapProvider>
-					<UserContext.Consumer>
-						{(userValue) => {
-							return (
-								<Web3Context.Consumer>
-									{(web3Value) => {
-										return (
-											<MapContext.Consumer>
-												{(mapValue) => {
-													return (
-														<Router>
-															<div className="App">
-																<ReactNotification />
-																<NavBar></NavBar>
-																<BannerNotification></BannerNotification>
-																<div className="o-container">
-																	{/* <Route path="/map/" component={Map}></Route>  TODO: KYC - Remove comment */}
+		<Suspense fallback="loading">
+			<UserProvider>
+				<Web3Provider>
+					<MapProvider>
+						<UserContext.Consumer>
+							{(userValue) => {
+								return (
+									<Web3Context.Consumer>
+										{(web3Value) => {
+											return (
+												<MapContext.Consumer>
+													{(mapValue) => {
+														return (
+															<Router>
+																<div className="App">
+																	<ReactNotification />
+																	<NavBar></NavBar>
+																	<BannerNotification></BannerNotification>
+																	<div className="o-container">
+																		{/* <Route path="/map/" component={Map}></Route>  TODO: KYC - Remove comment */}
+																	</div>
+																	<Switch>
+																		{/* <Route path="/" exact component={Home}></Route> */}
+																		<Route exact path="/">
+																			{userValue.state.isLoggedIn ? (
+																				<Redirect to="/profile" />
+																			) : (
+																				<Redirect to="/signup" />
+																			)}
+																			{/* TODO: KYC - Redirect to Redirect to="/map/discover" */}
+																		</Route>
+																		{/* <Route path="/map/discover" component={Discover}></Route>
+																		<Route path="/map/overview" component={Overview}></Route>
+																		<Route path="/map/lands" component={Lands}></Route>
+																		<Route path="/map/land/:id" component={Land}></Route>  TODO: KYC - Remove comment */}
+																		<Route path="/profile" component={Profile}></Route>
+																		{/* <Route path="/activity" component={Activity}></Route>  TODO: KYC - Remove comment */}
+																		<Route path="/login" component={Login}></Route>
+																		<Route path="/signup" component={Signup}></Route>
+																		{/* <Route path="/buy-tokens" component={BuyTokens}></Route>
+																		<Route path="/indacoin-response" component={IndacoinResponse}></Route>  TODO: KYC - Remove comment */}
+																	</Switch>
+																	<Footer></Footer>
 																</div>
-																<Switch>
-																	{/* <Route path="/" exact component={Home}></Route> */}
-																	<Route exact path="/">
-																		{userValue.state.isLoggedIn ? (
-																			<Redirect to="/profile" />
-																		) : (
-																			<Redirect to="/signup" />
-																		)}
-																		{/* TODO: KYC - Redirect to Redirect to="/map/discover" */}
-																	</Route>
-																	{/* <Route path="/map/discover" component={Discover}></Route>
-																	<Route path="/map/overview" component={Overview}></Route>
-																	<Route path="/map/lands" component={Lands}></Route>
-																	<Route path="/map/land/:id" component={Land}></Route>  TODO: KYC - Remove comment */}
-																	<Route path="/profile" component={Profile}></Route>
-																	{/* <Route path="/activity" component={Activity}></Route>  TODO: KYC - Remove comment */}
-																	<Route path="/login" component={Login}></Route>
-																	<Route path="/signup" component={Signup}></Route>
-																	{/* <Route path="/buy-tokens" component={BuyTokens}></Route>
-																	<Route path="/indacoin-response" component={IndacoinResponse}></Route>  TODO: KYC - Remove comment */}
-																</Switch>
-																<Footer></Footer>
-															</div>
-														</Router>
-													);
-												}}
-											</MapContext.Consumer>
-										);
-									}}
-								</Web3Context.Consumer>
-							);
-						}}
-					</UserContext.Consumer>
-				</MapProvider>
-			</Web3Provider>
-		</UserProvider>
+															</Router>
+														);
+													}}
+												</MapContext.Consumer>
+											);
+										}}
+									</Web3Context.Consumer>
+								);
+							}}
+						</UserContext.Consumer>
+					</MapProvider>
+				</Web3Provider>
+			</UserProvider>
+		</Suspense>
 	);
 }
 
