535b9d9d9e16f97bee6f2fab0d7b43da00ede7c9
Marco Gianni <defigodss@gmail.com>
2021-04-09 16:32:51 +0200

    added Suspense

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
@@ -6,13 +6,36 @@ import * as serviceWorker from './serviceWorker'
 
 import { BannerCounterContextProvider } from 'context/BannerCounterContext'
 import { NewMapContextProvider } from 'context/NewMapContext'
+import { NewUserContextProvider } from 'context/NewUserContext'
+
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+const Preloading = () => {
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <CircularProgress color="#2d245b" />
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <NewMapContextProvider>
-    <BannerCounterContextProvider>
-      <App />
-    </BannerCounterContextProvider>
-  </NewMapContextProvider>,
+  <Suspense fallback={<Preloading />}>
+    <NewUserContextProvider>
+      <NewMapContextProvider>
+        <BannerCounterContextProvider>
+          <App />
+        </BannerCounterContextProvider>
+      </NewMapContextProvider>
+    </NewUserContextProvider>
+  </Suspense>,
 
   document.getElementById('root')
 )
